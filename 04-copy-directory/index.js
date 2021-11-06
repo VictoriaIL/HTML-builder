a6bd2109970e8f71cf542c3336f600a7cf889cd7
Victoria Ilyina <47503735+VictoriaIL@users.noreply.github.com>
2021-11-07 01:58:43 +0300

    add: add solution for the 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'files');
+const dir = './04-copy-directory/files-copy';
 
+fs.mkdir(dir, {recursive: true}, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Directory is created.");
+});
+
+fs.readdir(filePath, (err, files) => {
+    files.forEach(file => {
+        fs.copyFile(path.join(__dirname, '/files/' + file), path.join(__dirname, 'files-copy/' + file), (err) => {
+        });
+    });
+});
